dbe4284f9b1a1406131800b0dee27fafe66da9c6
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-10-25 22:37:34 -0300

    Adiciona tipagens de png e rotas

---
+++ src/@types/navigate.d.ts
@@ -0,0 +1,16 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      signIn: undefined
+      signUp: undefined
+      home: undefined
+      details: { idProduct: string }
+      detailsMyProduct: { idProduct: string }
+      myProducts: undefined
+      newProduct: undefined
+      editProduct: undefined
+      homeTab: undefined
+      resumeProduct: undefined
+    }
+  }
+}
