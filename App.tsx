87c047dee3b55f328701c12c214048703b522841
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-10-25 22:36:53 -0300

    Adiciona Providers

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { Routes } from '@routes/index'
-import { NativeBaseProvider, StatusBar } from 'native-base'
+import { NativeBaseProvider } from 'native-base'
 
 import {
   useFonts,
@@ -8,18 +8,25 @@ import {
 } from '@expo-google-fonts/karla'
 import { Theme } from './src/theme'
 import { Loading } from '@components/Loading'
+import { StatusBar } from 'react-native'
+import { AuthContextProvider } from './src/context/AuthContext'
+import { ProductsContextProvider } from './src/context/ProductsContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
 
   return (
-    <NativeBaseProvider theme={Theme}>
-      {fontsLoaded ? <Routes /> : <Loading />}
-      <StatusBar
-        barStyle={'light-content'}
-        backgroundColor={'transparent'}
-        translucent
-      />
-    </NativeBaseProvider>
+    <AuthContextProvider>
+      <ProductsContextProvider>
+        <NativeBaseProvider theme={Theme}>
+          {fontsLoaded ? <Routes /> : <Loading />}
+          <StatusBar
+            barStyle={'dark-content'}
+            backgroundColor={'#EDECEE'}
+            translucent
+          />
+        </NativeBaseProvider>
+      </ProductsContextProvider>
+    </AuthContextProvider>
   )
 }
